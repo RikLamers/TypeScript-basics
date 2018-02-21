e9eaa0325012c0eedbcae14e977bb2467c45cc88
Rik Lamers <info@riklamers.com>
2018-02-21 20:54:20 +0100

    Goooooood

--- app.js
+++ app.js
@@ -1,129 +1,31 @@
 "use strict";
-// string
-var myName = 'Rik';
-// myName = 30;
-// number
-var myAge = 21;
-// myAge = 'Rik';
-// boolean
-var hasHobbies = false;
-// hasHobbies = 1;
-// assign types
-var myRealAge;
-myRealAge = 27;
-// myRealAge = '27';
-// array
-var hobbies = ['Cooking', 'Coding'];
-hobbies = [100];
-// tuples
-var address = ['Superstreet', 99];
-// address = [99, "Superstreet"];
-// enums
-var Color;
-(function (Color) {
-    Color[Color["Gray"] = 0] = "Gray";
-    Color[Color["Green"] = 100] = "Green";
-    Color[Color["Blue"] = 101] = "Blue";
-})(Color || (Color = {}));
-var myColor = Color.Blue;
-console.log(myColor);
-// any EXECPTION!
-var car = 'BWM';
-console.log(car);
-car = { Brand: 'BMW', sersies: 3 };
-console.log(car);
-// functions
-function returnMyName() {
-    return myName;
-}
-console.log(returnMyName());
-// void WANNEER JE NIKS RETURNED!
-function sayHello() {
-    console.log('Hello');
-    // return 'hello';
-}
-// argument types
-function multiply(value1, value2) {
-    return value1 * value2;
-}
-console.log(multiply(2, 3));
-// function types
-var myMultiply;
-// myMultiply = sayHello();
-// myMultiply();
-myMultiply = multiply;
-console.log(myMultiply(2, 5));
-// objects
-var userData = {
-    name: 'Rik',
-    age: 21,
+var double = function (value) {
+    return value * 2;
 };
-// userData = {
-//     a: 'hello', --> name: 'Hello'
-//     b: 22
-// };
-// complex object
-var complex = {
-    data: [100, 3.99, 10],
-    output: function (all) {
-        return this.data;
-    }
+console.log(double(10));
+// Exercise 2 - If only we could provide some default values...
+var greet = function (name) {
+    if (name === void 0) { name = 'Rik'; }
+    console.log("Hello, " + name);
 };
-var complex2 = {
-    data: [100, 3.99, 10],
-    output: function (all) {
-        return this.data;
-    }
+greet();
+greet("Anna");
+// Exercise 3 - Isn't there a shorter way to get all these Values?
+var numbers = [-3, 33, 38, 5];
+console.log(Math.min.apply(Math, numbers));
+// Exercise 4 - I have to think about Exercise 3 ...
+var newArray = [55, 20];
+(_a = Array.prototype).push.apply(_a, numbers);
+console.log(newArray);
+// Exercise 5 - That's a well-constructed array.
+var testResults = [3.89, 2.99, 1.38];
+var result1 = testResults[0], result2 = testResults[1], result3 = testResults[2];
+console.log(result1, result2, result3);
+// Exercise 6 - And a well-constructed object!
+var scientist = {
+    firstName: "Will",
+    experience: 12
 };
-//union types
-var myRealRealAge = 21;
-myRealRealAge = '21';
-// myRealRealAge = true;
-// check types
-var finalValue = 'A string';
-if (typeof finalValue === 'string') {
-    console.log(finalValue);
-}
-// never
-function neverReturns() {
-    throw new Error('An error');
-}
-// nullable types
-var canBeNull = 12;
-canBeNull = null;
-var canAlsoBeNull;
-canAlsoBeNull = null;
-var canThisBeAny = null;
-canThisBeAny = 12;
-// spread operator
-console.log('REST & SPREAD OPERATOR');
-var numbers = [1, 10, 99, -5];
-console.log(Math.max(33, 99, 10, -3));
-console.log(Math.max.apply(Math, numbers));
-function makeArrayFunction(arg1, arg2) {
-    return [arg1, arg2];
-}
-// Wanneer je niet weet hoeveel arguments je als parameter mee geeft
-function makeArrayFunction1() {
-    var args = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        args[_i] = arguments[_i];
-    }
-    return [args];
-}
-console.log(makeArrayFunction1(1, 2));
-//DESTRUCTURING
-var myHobbies = ['Cooking', 'Coding'];
-var hobby1 = myHobbies[0], hobby2 = myHobbies[1];
-console.log(hobby1, hobby2);
-var userData1 = {
-    userName: 'Rik',
-    age: 21
-};
-var userName = userData1.userName, age = userData1.age;
-console.log(userName, age);
-// TEMPLATE LITERALS
-var NameUser = 'Rik';
-// const greetz = 'Hello, my name is ' + NameUser;
-var greetz = " This is a heading!\nI am " + NameUser;
-console.log(greetz);
+var firstName = scientist.firstName, experience = scientist.experience;
+console.log(firstName, experience);
+var _a;
--- app.ts
+++ app.ts
@@ -1,169 +1,33 @@
-// string
-let myName: string = 'Rik';
-// myName = 30;
-
-// number
-let myAge: number = 21;
-// myAge = 'Rik';
-
-// boolean
-let hasHobbies: boolean = false;
-// hasHobbies = 1;
-
-// assign types
-let myRealAge: number;
-myRealAge = 27;
-// myRealAge = '27';
-
-// array
-let hobbies: any[] = ['Cooking', 'Coding'];
-hobbies = [100];
-
-// tuples
-let address: [string, number] = ['Superstreet', 99];
-// address = [99, "Superstreet"];
-
-// enums
-enum Color {
-    Gray,
-    Green = 100,
-    Blue
-}
-
-let myColor: Color = Color.Blue;
-console.log(myColor);
-
-// any EXECPTION!
-let car: any = 'BWM';
-console.log(car);
-car = {Brand: 'BMW', sersies: 3};
-console.log(car);
-
-// functions
-function returnMyName(): string {
-    return myName;
-}
-
-console.log(returnMyName());
-
-// void WANNEER JE NIKS RETURNED!
-function sayHello(): void {
-    console.log('Hello');
-    // return 'hello';
-}
-
-// argument types
-function multiply (value1: number, value2: number): number {
-    return value1 * value2;
-}
-
-console.log(multiply(2, 3));
-
-// function types
-let myMultiply: (val1: number, val2: number) => number;
-// myMultiply = sayHello();
-// myMultiply();
-myMultiply = multiply;
-console.log(myMultiply(2, 5));
-
-// objects
-let userData: { name: string, age: number } = {
-    name: 'Rik',
-    age: 21,
+const double = (value: number): number => {
+    return value * 2;
 };
+console.log(double(10));
 
-// userData = {
-//     a: 'hello', --> name: 'Hello'
-//     b: 22
-// };
-
-// complex object
-let complex: { data: number[], output: (all: boolean) => number[] } = {
-    data: [100, 3.99, 10],
-    output: function (all: boolean): number[] {
-        return this.data;
-    }
+// Exercise 2 - If only we could provide some default values...
+const greet = (name: string = 'Rik'): void => {
+    console.log("Hello, " + name);
 };
-// complex = {};
+greet();
+greet("Anna");
 
-//type alias
-type Complex = { data: number[], output: (all: boolean) => number[] };
+// Exercise 3 - Isn't there a shorter way to get all these Values?
+const numbers = [-3, 33, 38, 5];
+console.log(Math.min(...numbers));
 
-let complex2: Complex = {
-    data: [100, 3.99, 10],
-    output: function (all: boolean): number[] {
-        return this.data;
-    }
+// Exercise 4 - I have to think about Exercise 3 ...
+const newArray = [55, 20];
+Array.prototype.push(...numbers);
+console.log(newArray);
+
+// Exercise 5 - That's a well-constructed array.
+const testResults = [3.89, 2.99, 1.38];
+const [result1, result2, result3] = testResults;
+console.log(result1, result2, result3);
+
+// Exercise 6 - And a well-constructed object!
+const scientist = {
+    firstName: "Will",
+    experience: 12
 };
-
-//union types
-let myRealRealAge: number | string = 21;
-myRealRealAge = '21';
-// myRealRealAge = true;
-
-// check types
-let finalValue = 'A string';
-if (typeof finalValue === 'string') {
-    console.log(finalValue);
-}
-
-// never
-function neverReturns(): never {
-    throw new Error('An error');
-}
-
-// nullable types
-let canBeNull: number | null = 12;
-canBeNull = null;
-let canAlsoBeNull;
-canAlsoBeNull = null;
-let canThisBeAny = null;
-canThisBeAny = 12;
-
-
-
-
-
-// spread operator
-console.log('REST & SPREAD OPERATOR');
-
-const numbers = [1, 10, 99, -5];
-
-console.log(Math.max(33, 99, 10, -3));
-console.log(Math.max(...numbers));
-
-function makeArrayFunction(arg1: number, arg2: number) {
-    return [arg1, arg2];
-}
-
-// Wanneer je niet weet hoeveel arguments je als parameter mee geeft
-function makeArrayFunction1(...args: number[]) {
-    return [args];
-}
-
-console.log(makeArrayFunction1(1, 2));
-
-
-//DESTRUCTURING
-
-const myHobbies = ['Cooking', 'Coding'];
-
-const [ hobby1, hobby2 ] = myHobbies;
-console.log(hobby1, hobby2);
-
-const userData1 ={
-    userName: 'Rik',
-    age: 21
-};
-
-const { userName, age } = userData1;
-console.log(userName, age);
-
-// TEMPLATE LITERALS
-
-const NameUser = 'Rik';
-// const greetz = 'Hello, my name is ' + NameUser;
-const greetz = ` This is a heading!
-I am ${NameUser}`;
-
-console.log(greetz);
+const { firstName, experience } = scientist;
+console.log(firstName, experience);
