9c5c420a2f81c782fc035d2322c0e68fcb7aa0eb
Rik Lamers <info@riklamers.com>
2018-02-21 21:43:07 +0100

    STATIC MAAAAAAN

--- app.ts
+++ app.ts
@@ -81,3 +81,30 @@ class Helpers {
 
 console.log(2 * Helpers.PI);
 console.log(Helpers.calcCircumFerence(8));
+
+
+
+// ABSTRACT CLASSES
+// THIS IS JUST HERE TO BE INHERIT
+abstract class Project {
+    public projectName: string = 'Default';
+    public budget: number = 1000;
+
+    abstract changeName(name: string): void;
+
+    public calcBudget() {
+        return this.budget * 2;
+    }
+
+}
+
+class ITProject extends Project {
+    changeName(name: string): void {
+        this.projectName = name;
+    }
+}
+
+let newProject = new ITProject();
+console.log(newProject);
+newProject.changeName('Super IT Project');
+console.log(newProject);
