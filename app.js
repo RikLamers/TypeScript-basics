6c230c4833c71fc833877f07b0eb3f860aa2934a
Rik Lamers <info@riklamers.com>
2018-02-27 00:14:54 +0100

    Interfaces

--- app.js
+++ app.js
@@ -1,129 +1,45 @@
 "use strict";
-// string
-var myName = 'Rik';
-// myName = 30;
-// number
-var myAge = 21;
-// myAge = 'Rik';
-// boolean
-var hasHobbies = false;
-// hasHobbies = 1;
-// assign types
-var myRealAge;
-myRealAge = 27;
-// myRealAge = '27';
-// array
-var hobbies = ['Cooking', 'Coding'];
-hobbies = [100];
-// tuples
-var address = ['Superstreet', 99];
-// address = [99, "Superstreet"];
-// enums
-var Color;
-(function (Color) {
-    Color[Color["Gray"] = 0] = "Gray";
-    Color[Color["Green"] = 100] = "Green";
-    Color[Color["Blue"] = 101] = "Blue";
-})(Color || (Color = {}));
-var myColor = Color.Blue;
-console.log(myColor);
-// any EXECPTION!
-var car = 'BWM';
-console.log(car);
-car = { Brand: 'BMW', sersies: 3 };
-console.log(car);
-// functions
-function returnMyName() {
-    return myName;
+function greet(person) {
+    console.log('Hello', person.firstName);
 }
-console.log(returnMyName());
-// void WANNEER JE NIKS RETURNED!
-function sayHello() {
-    console.log('Hello');
-    // return 'hello';
+function changeName(person) {
+    person.firstName = 'Anne';
 }
-// argument types
-function multiply(value1, value2) {
-    return value1 * value2;
-}
-console.log(multiply(2, 3));
-// function types
-var myMultiply;
-// myMultiply = sayHello();
-// myMultiply();
-myMultiply = multiply;
-console.log(myMultiply(2, 5));
-// objects
-var userData = {
-    name: 'Rik',
+var person = {
+    firstName: 'Rik',
+    hobbies: ['Cooking', 'Coding'],
+    greet: function (lastName) {
+        console.log("Hi I am " + this.firstName + " " + lastName);
+    },
+};
+// greet({firstname: 'Rik', age: 21});
+changeName(person);
+greet(person);
+person.greet('Anything');
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    Person.prototype.greet = function (lastName) {
+        console.log("Hi I am " + this.firstName + " " + lastName);
+    };
+    ;
+    return Person;
+}());
+var myPerson = new Person();
+myPerson.firstName = 'Rik';
+myPerson.lastName = 'Anythingg';
+greet(myPerson);
+myPerson.greet('Anything');
+var myDoubleFunction;
+myDoubleFunction = function (number1, number2) {
+    return (number1 + number2) * 2;
+};
+console.log(myDoubleFunction(10, 20));
+var oldPerson = {
     age: 21,
-};
-// userData = {
-//     a: 'hello', --> name: 'Hello'
-//     b: 22
-// };
-// complex object
-var complex = {
-    data: [100, 3.99, 10],
-    output: function (all) {
-        return this.data;
+    firstName: 'Rik',
+    greet: function (lastName) {
+        console.log('Hello');
     }
 };
-var complex2 = {
-    data: [100, 3.99, 10],
-    output: function (all) {
-        return this.data;
-    }
-};
-//union types
-var myRealRealAge = 21;
-myRealRealAge = '21';
-// myRealRealAge = true;
-// check types
-var finalValue = 'A string';
-if (typeof finalValue === 'string') {
-    console.log(finalValue);
-}
-// never
-function neverReturns() {
-    throw new Error('An error');
-}
-// nullable types
-var canBeNull = 12;
-canBeNull = null;
-var canAlsoBeNull;
-canAlsoBeNull = null;
-var canThisBeAny = null;
-canThisBeAny = 12;
-// spread operator
-console.log('REST & SPREAD OPERATOR');
-var numbers = [1, 10, 99, -5];
-console.log(Math.max(33, 99, 10, -3));
-console.log(Math.max.apply(Math, numbers));
-function makeArrayFunction(arg1, arg2) {
-    return [arg1, arg2];
-}
-// Wanneer je niet weet hoeveel arguments je als parameter mee geeft
-function makeArrayFunction1() {
-    var args = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        args[_i] = arguments[_i];
-    }
-    return [args];
-}
-console.log(makeArrayFunction1(1, 2));
-//DESTRUCTURING
-var myHobbies = ['Cooking', 'Coding'];
-var hobby1 = myHobbies[0], hobby2 = myHobbies[1];
-console.log(hobby1, hobby2);
-var userData1 = {
-    userName: 'Rik',
-    age: 21
-};
-var userName = userData1.userName, age = userData1.age;
-console.log(userName, age);
-// TEMPLATE LITERALS
-var NameUser = 'Rik';
-// const greetz = 'Hello, my name is ' + NameUser;
-var greetz = " This is a heading!\nI am " + NameUser;
-console.log(greetz);
+console.log(oldPerson);
--- app.ts
+++ app.ts
@@ -1,169 +1,70 @@
-// string
-let myName: string = 'Rik';
-// myName = 30;
-
-// number
-let myAge: number = 21;
-// myAge = 'Rik';
-
-// boolean
-let hasHobbies: boolean = false;
-// hasHobbies = 1;
-
-// assign types
-let myRealAge: number;
-myRealAge = 27;
-// myRealAge = '27';
-
-// array
-let hobbies: any[] = ['Cooking', 'Coding'];
-hobbies = [100];
-
-// tuples
-let address: [string, number] = ['Superstreet', 99];
-// address = [99, "Superstreet"];
-
-// enums
-enum Color {
-    Gray,
-    Green = 100,
-    Blue
+interface namedPerson {
+    firstName: string,
+    age?: number,
+    [propName: string]: any,
+    greet(lastName: string): void;
 }
 
-let myColor: Color = Color.Blue;
-console.log(myColor);
-
-// any EXECPTION!
-let car: any = 'BWM';
-console.log(car);
-car = {Brand: 'BMW', sersies: 3};
-console.log(car);
-
-// functions
-function returnMyName(): string {
-    return myName;
+function greet(person: namedPerson) {
+    console.log('Hello', person.firstName);
 }
 
-console.log(returnMyName());
-
-// void WANNEER JE NIKS RETURNED!
-function sayHello(): void {
-    console.log('Hello');
-    // return 'hello';
+function changeName(person: namedPerson) {
+    person.firstName = 'Anne'
 }
 
-// argument types
-function multiply (value1: number, value2: number): number {
-    return value1 * value2;
+const person: namedPerson = {
+    firstName: 'Rik',
+    hobbies: ['Cooking', 'Coding'],
+    greet(lastName: string) {
+        console.log(`Hi I am ${this.firstName} ${lastName}`)
+    },
+};
+
+// greet({firstname: 'Rik', age: 21});
+changeName(person);
+greet(person);
+person.greet('Anything');
+
+class Person implements namedPerson {
+    firstName: string;
+    lastName: string;
+    greet(lastName: string) {
+        console.log(`Hi I am ${this.firstName} ${lastName}`);
+    };
 }
 
-console.log(multiply(2, 3));
+const myPerson = new Person();
+myPerson.firstName = 'Rik';
+myPerson.lastName = 'Anythingg';
+greet(myPerson);
+myPerson.greet('Anything');
 
-// function types
-let myMultiply: (val1: number, val2: number) => number;
-// myMultiply = sayHello();
-// myMultiply();
-myMultiply = multiply;
-console.log(myMultiply(2, 5));
+// Function Types
 
-// objects
-let userData: { name: string, age: number } = {
-    name: 'Rik',
+interface DoubleValueFunc {
+    (number1: number, number2: number): number
+}
+
+let myDoubleFunction: DoubleValueFunc;
+myDoubleFunction = function(number1: number, number2: number): number {
+    return (number1 + number2) * 2;
+}
+
+console.log(myDoubleFunction(10, 20));
+
+// Interface Inheritance
+
+interface agedPerson extends namedPerson {
+    age: number;
+}
+
+const oldPerson: agedPerson = {
     age: 21,
-};
-
-// userData = {
-//     a: 'hello', --> name: 'Hello'
-//     b: 22
-// };
-
-// complex object
-let complex: { data: number[], output: (all: boolean) => number[] } = {
-    data: [100, 3.99, 10],
-    output: function (all: boolean): number[] {
-        return this.data;
-    }
-};
-// complex = {};
-
-//type alias
-type Complex = { data: number[], output: (all: boolean) => number[] };
-
-let complex2: Complex = {
-    data: [100, 3.99, 10],
-    output: function (all: boolean): number[] {
-        return this.data;
+    firstName: 'Rik',
+    greet(lastName: string): void {
+        console.log('Hello');
     }
 };
 
-//union types
-let myRealRealAge: number | string = 21;
-myRealRealAge = '21';
-// myRealRealAge = true;
-
-// check types
-let finalValue = 'A string';
-if (typeof finalValue === 'string') {
-    console.log(finalValue);
-}
-
-// never
-function neverReturns(): never {
-    throw new Error('An error');
-}
-
-// nullable types
-let canBeNull: number | null = 12;
-canBeNull = null;
-let canAlsoBeNull;
-canAlsoBeNull = null;
-let canThisBeAny = null;
-canThisBeAny = 12;
-
-
-
-
-
-// spread operator
-console.log('REST & SPREAD OPERATOR');
-
-const numbers = [1, 10, 99, -5];
-
-console.log(Math.max(33, 99, 10, -3));
-console.log(Math.max(...numbers));
-
-function makeArrayFunction(arg1: number, arg2: number) {
-    return [arg1, arg2];
-}
-
-// Wanneer je niet weet hoeveel arguments je als parameter mee geeft
-function makeArrayFunction1(...args: number[]) {
-    return [args];
-}
-
-console.log(makeArrayFunction1(1, 2));
-
-
-//DESTRUCTURING
-
-const myHobbies = ['Cooking', 'Coding'];
-
-const [ hobby1, hobby2 ] = myHobbies;
-console.log(hobby1, hobby2);
-
-const userData1 ={
-    userName: 'Rik',
-    age: 21
-};
-
-const { userName, age } = userData1;
-console.log(userName, age);
-
-// TEMPLATE LITERALS
-
-const NameUser = 'Rik';
-// const greetz = 'Hello, my name is ' + NameUser;
-const greetz = ` This is a heading!
-I am ${NameUser}`;
-
-console.log(greetz);
+console.log(oldPerson);
